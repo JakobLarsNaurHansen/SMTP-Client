16b690f282716c08f4902d083ba7e453ccdeb4a1
Jakob Hansen <jakob_naur@live.dk>
2023-03-06 11:35:09 +0100

    initialcommit

---
+++ src/SMTPConnection.java
@@ -0,0 +1,106 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.util.StringTokenizer;
+
+/**
+ * Open an SMTP connection to a mailserver and send one mail.
+ *
+ */
+public class SMTPConnection {
+    /* The socket to the server */
+    private Socket connection;
+
+    /* Streams for reading and writing the socket */
+    private BufferedReader fromServer;
+    private DataOutputStream toServer;
+
+    private static final int SMTP_PORT = 25;
+    private static final String CRLF = "\r\n";
+
+    /* Are we connected? Used in close() to determine what to do. */
+    private boolean isConnected = false;
+
+    /* Create an SMTPConnection object. Create the socket and the
+       associated streams. Initialize SMTP connection. */
+    public SMTPConnection(Envelope envelope) throws IOException {
+        connection = /* Fill in */;
+        fromServer = /* Fill in */;
+        toServer =   /* Fill in */;
+
+        /* Fill in */
+	/* Read a line from server and check that the reply code is 220.
+	   If not, throw an IOException. */
+        /* Fill in */
+
+	/* SMTP handshake. We need the name of the local machine.
+	   Send the appropriate SMTP handshake command. */
+        String localhost = /* Fill in */;
+        sendCommand( /* Fill in */ );
+
+        isConnected = true;
+    }
+
+    /* Send the message. Write the correct SMTP-commands in the
+       correct order. No checking for errors, just throw them to the
+       caller. */
+    public void send(Envelope envelope) throws IOException {
+        /* Fill in */
+	/* Send all the necessary commands to send a message. Call
+	   sendCommand() to do the dirty work. Do _not_ catch the
+	   exception thrown from sendCommand(). */
+        /* Fill in */
+    }
+
+    /* Close the connection. First, terminate on SMTP level, then
+       close the socket. */
+    public void close() {
+        isConnected = false;
+        try {
+            sendCommand("QUIT", 221);
+            connection.close();
+        } catch (IOException e) {
+            System.out.println("Unable to close connection: " + e);
+            isConnected = true;
+        }
+    }
+
+    /* Send an SMTP command to the server. Check that the reply code is
+       what is is supposed to be according to RFC 821. */
+    private void sendCommand(String command, int rc) throws IOException {
+
+        //Write Command to server
+        System.out.println(command);
+
+        // Read reply from server
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String str;
+        String response;
+        while ((str = bufferedReader.readLine()) != null) {
+            response.append(str);
+        }
+        /* Parse server reply code */
+        int replyCode = parseReply(response);
+
+        /* Check that the server's reply code matches the expected code */
+        if (replyCode != rc) {
+            throw new IOException("Unexpected reply code: " + replyCode);
+        }
+    }
+
+    /* Parse the reply line from the server. Returns the reply code. */
+    private int parseReply(String reply) {
+        StringTokenizer tokens = new StringTokenizer(reply, " ");
+        String codeStr = tokens.nextToken();
+        int code = Integer.parseInt(codeStr);
+        return code;
+    }
+
+    /* Destructor. Closes the connection if something bad happens. */
+    protected void finalize() throws Throwable {
+        if(isConnected) {
+            close();
+        }
+        super.finalize();
+    }
+}
