299dde020d85819e609accfefbed8e9ad18f085a
Henrik Zenkert <henrik.zenkert@gmail.com>
2023-03-10 11:55:46 +0100

    Encode images

---
+++ src/Base64Encoder.java
@@ -0,0 +1,21 @@
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.io.*;
+
+public class Base64Encoder {
+    public static String encodeBase64(String path) throws IOException {
+        byte[] imageBytes = Files.readAllBytes(new File(path).toPath());
+        return new String(Base64.getEncoder().encode(imageBytes), StandardCharsets.UTF_8);
+    }
+
+    public static void main(String[] args) {
+        try {
+            String result = encodeBase64("../dog.jpeg");
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
