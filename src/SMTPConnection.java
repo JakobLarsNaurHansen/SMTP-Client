5d1ddb74245981483315366cf82003ae442945c8
Henrik Zenkert <henrik.zenkert@gmail.com>
2023-03-13 20:37:00 +0100

    fixed message class

--- src/SMTPConnection.java
+++ src/SMTPConnection.java
@@ -13,7 +13,7 @@ public class SMTPConnection {
     final private BufferedReader fromServer;
     final private DataOutputStream toServer;
 
-    private static final int SMTP_PORT = 25;
+    private static final int SMTP_PORT = 2526;
     private static final String CRLF = "\r\n";
 
     /* Are we connected? Used in close() to determine what to do. */
